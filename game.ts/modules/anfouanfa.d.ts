078063c7b5c5599526b97ad8222f08fdb4c790f4
turb0Code <turb000.max.10@gmail.com>
2025-02-18 02:30:07 +0100

    Added bars skeleton.

---
+++ game.ts/modules/anfouanfa.d.ts
@@ -0,0 +1,2 @@
+declare const śmieszne_coś: (ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => void;
+export default śmieszne_coś;
---
+++ game.ts/script/grawitacja.d.ts
@@ -0,0 +1,2 @@
+declare const grawituj_się_stary: (ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, element: any) => void;
+export default grawituj_się_stary;
